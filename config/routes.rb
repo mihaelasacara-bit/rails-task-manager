1ed6349646600b3bcc8398d85c12791e60944296
Mihaela Sacara <mihaelasacara@gmail.com>
2026-04-07 15:26:41 +0200

    fixed completed checkbox, added delete function, refactored routes, controller, form

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new", as: :new_task
-  post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
-  get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+  resources :tasks
 end
